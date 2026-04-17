c5b6f9784fdd7bbde5919e3dd4addf6365713add
dschaym <dschay@gmail.com>
2026-04-17 13:19:15 -0600

    Agregar archivos de proyecto.

---
+++ 1/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Filas: ");
+int filas = int.Parse(Console.ReadLine());
+Console.Write("Columnas: ");
+int columnas = int.Parse(Console.ReadLine());
+
+int[,] matriz = new int[filas, columnas];
+
+for (int i = 0; i < filas; i++)
+{
+    for (int j = 0; j < columnas; j++)
+    {
+        Console.Write("Valor: ");
+        matriz[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+
+Console.WriteLine("Matriz: ");
+for (int i= 0; i < filas; i ++)
+{
+    for (int j = 0; j < columnas; j++)
+    {
+        Console.Write(matriz[i,j]+ " | ");
+    }
+    Console.WriteLine();
+}

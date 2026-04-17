39f34090dc869e339f011d4ddacd2d2d10d9caab
dschaym <dschay@gmail.com>
2026-04-17 14:38:23 -0600

    Ejercicios terminados

---
+++ 4.1/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Filas: ");
+int filas = int.Parse(Console.ReadLine());
+Console.Write("Columnas: ");
+int columnas = int.Parse(Console.ReadLine());
+
+int[,] matriz = new int[filas, columnas];
+
+bool numeroMayor = false;
+int mayor = 0;
+
+for (int i = 0; i < filas; i++)
+{
+    for (int j = 0; j < columnas; j++)
+    {
+        Console.Write("Valor: ");
+        matriz[i, j] = int.Parse(Console.ReadLine());
+
+        if (!numeroMayor)
+        {
+            mayor = matriz[i, j];
+            numeroMayor = true;
+        }
+        else if (matriz[i, j] > mayor)
+        {
+            mayor = matriz[i, j];
+        }
+    }
+}
+Console.WriteLine("Número mayor: " + mayor);
---
+++ 5/Program.cs
@@ -0,0 +1,38 @@
+﻿
+Console.Write("Filas: ");
+int filas = int.Parse(Console.ReadLine());
+Console.Write("Columnas: ");
+int columnas = int.Parse(Console.ReadLine());
+
+int[,] matriz = new int[filas, columnas];
+
+for (int i = 0; i < filas; i++)
+{
+    for (int j = 0; j < columnas; j++)
+    {
+        Console.Write($"Valor [{i},{j}]: ");
+        matriz[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+
+Console.Write("Número a buscar: ");
+int buscar = int.Parse(Console.ReadLine());
+
+bool encontrado = false;
+
+for (int i = 0; i < filas; i++)
+{
+    for (int j = 0; j < columnas; j++)
+    {
+        if (matriz[i, j] == buscar)
+        {
+            Console.WriteLine($"Encontrado en fila {i}, columna {j}");
+            encontrado = true;
+        }
+    }
+}
+
+if (!encontrado)
+{
+    Console.WriteLine("Número no encontrado");
+}

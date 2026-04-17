64a878f9b384faa539968bbf396de579816833c4
dschaym <dschay@gmail.com>
2026-04-17 13:39:17 -0600

    Ejercicio 2

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Write("Filas: ");
+int filas = int.Parse(Console.ReadLine());
+Console.Write("Columnas: ");
+int columnas = int.Parse(Console.ReadLine());
+
+int[,] matriz = new int[filas, columnas];
+
+for (int i = 0; i < filas; i++)
+{
+    for (int j = 0; j < columnas; j++)
+    {
+        Console.Write("Valor: ");
+        matriz[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+
+Console.Write("Buscar fila: ");
+int fila = int.Parse(Console.ReadLine());
+
+for (int j = 0; j < columnas; j++)
+{
+    Console.Write(matriz[fila, j] + " ");
+}

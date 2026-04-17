dc0f4acf74e5ef9497c0830504b42fcbbe9ef575
dschaym <dschay@gmail.com>
2026-04-17 13:48:39 -0600

    Ejercicio 3

---
+++ 3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Filas: ");
+int filas = int.Parse(Console.ReadLine());
+Console.Write("Columnas: ");
+int columnas = int.Parse(Console.ReadLine());
+
+int[,] matriz = new int[filas, columnas];
+int suma = 0;
+
+for (int i = 0; i < filas; i++)
+{
+    for (int j = 0; j < columnas; j++)
+    {
+        Console.Write($"Valor [{i},{j}]: ");
+        matriz[i, j] = int.Parse(Console.ReadLine());
+        suma += matriz[i, j];
+    }
+}
+
+Console.WriteLine("Suma total: " + suma);
